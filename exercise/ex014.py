b7f1c7e608f18ecd5c26c807ec0489c6531a34c0
Clayson Roberto Eufrasio <clayson.eufrasio.dev@gmail.com>
2025-09-18 07:14:46 -0300

    Exercício: conversão de Celsius para Fahrenheit

---
+++ exercise/ex014.py
@@ -0,0 +1,8 @@
+'''
+Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+'''
+
+c = float(input('Enter the temperature in celsius: '))
+f = (c * 9) / 5 + 32
+
+print('The temperature in Fahrenheit is: ', f)
